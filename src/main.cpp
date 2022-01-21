e6f499142d41a9c8ec78dd0c4791464b0917830a
tashaxing <tashaxing123@163.com>
2022-01-21 18:19:50 +0800

    create project structure

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "stream_server.h"
+
+int main()
+{
+    return 0;
+}
---
+++ src/stream_server.h
@@ -0,0 +1,26 @@
+#ifndef _STREAM_SERVER_H_
+#define _STREAM_SERVER_H_
+
+#include "RTSPServer.hh"
+
+class DynamicRTSPServer: public RTSPServer 
+{
+public:
+  static DynamicRTSPServer* createNew(UsageEnvironment& env, Port ourPort,
+                      UserAuthenticationDatabase* authDatabase,
+                      unsigned reclamationTestSeconds = 65);
+
+protected:
+  DynamicRTSPServer(UsageEnvironment& env, int ourSocketIPv4, int ourSocketIPv6, Port ourPort,
+            UserAuthenticationDatabase* authDatabase, unsigned reclamationTestSeconds);
+  // called only by createNew();
+  virtual ~DynamicRTSPServer();
+
+protected: // redefined virtual functions
+  virtual void lookupServerMediaSession(char const* streamName,
+                    lookupServerMediaSessionCompletionFunc* completionFunc,
+                    void* completionClientData,
+                    Boolean isFirstLookupInSession);
+};
+
+#endif
