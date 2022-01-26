8bc0868e8d92636fbbad8552f44bfbae20188d6d
tashaxing <tashaxing123@163.com>
2022-01-26 10:21:29 +0800

    update print

--- src/main.cpp
+++ src/main.cpp
@@ -28,47 +28,47 @@ int main(int argc, char** argv)
         rtspServer = DynamicRTSPServer::createNew(*env, rtspServerPortNum, authDB);
     }
     if (rtspServer == NULL) {
-        *env << "Failed to create RTSP server: " << env->getResultMsg() << "\n";
+        std::cerr << "Failed to create RTSP server: " << env->getResultMsg() << "\n";
         exit(1);
     }
 
-    *env << "LIVE555 Media Server\n";
-    *env << "\tversion " << MEDIA_SERVER_VERSION_STRING
+    std::cout << "LIVE555 Media Server\n";
+    std::cout << "\tversion " << MEDIA_SERVER_VERSION_STRING
         << " (LIVE555 Streaming Media library version "
         << LIVEMEDIA_LIBRARY_VERSION_STRING << ").\n";
 
-    *env << "Play streams from this server using the URL\n";
+    std::cout << "Play streams from this server using the URL\n";
     if (weHaveAnIPv4Address(*env)) {
         char* rtspURLPrefix = rtspServer->ipv4rtspURLPrefix();
-        *env << "\t" << rtspURLPrefix << "<filename>\n";
+        std::cout << "\t" << rtspURLPrefix << "<filename>\n";
         delete[] rtspURLPrefix;
         if (weHaveAnIPv6Address(*env)) *env << "or\n";
     }
     if (weHaveAnIPv6Address(*env)) {
         char* rtspURLPrefix = rtspServer->ipv6rtspURLPrefix();
-        *env << "\t" << rtspURLPrefix << "<filename>\n";
+        std::cout << "\t" << rtspURLPrefix << "<filename>\n";
         delete[] rtspURLPrefix;
     }
-    *env << "where <filename> is a file present in the current directory.\n";
+    std::cout << "where <filename> is a file present in the current directory.\n";
 
-    *env << "Each file's type is inferred from its name suffix:\n";
-    *env << "\t\".264\" => a H.264 Video Elementary Stream file\n";
-    *env << "\t\".265\" => a H.265 Video Elementary Stream file\n";
-    *env << "\t\".aac\" => an AAC Audio (ADTS format) file\n";
-    *env << "\t\".ac3\" => an AC-3 Audio file\n";
-    *env << "\t\".amr\" => an AMR Audio file\n";
-    *env << "\t\".dv\" => a DV Video file\n";
-    *env << "\t\".m4e\" => a MPEG-4 Video Elementary Stream file\n";
-    *env << "\t\".mkv\" => a Matroska audio+video+(optional)subtitles file\n";
-    *env << "\t\".mp3\" => a MPEG-1 or 2 Audio file\n";
-    *env << "\t\".mpg\" => a MPEG-1 or 2 Program Stream (audio+video) file\n";
-    *env << "\t\".ogg\" or \".ogv\" or \".opus\" => an Ogg audio and/or video file\n";
-    *env << "\t\".ts\" => a MPEG Transport Stream file\n";
-    *env << "\t\t(a \".tsx\" index file - if present - provides server 'trick play' support)\n";
-    *env << "\t\".vob\" => a VOB (MPEG-2 video with AC-3 audio) file\n";
-    *env << "\t\".wav\" => a WAV Audio file\n";
-    *env << "\t\".webm\" => a WebM audio(Vorbis)+video(VP8) file\n";
-    *env << "See http://www.live555.com/mediaServer/ for additional documentation.\n";
+    std::cout << "Each file's type is inferred from its name suffix:\n";
+    std::cout << "\t\".264\" => a H.264 Video Elementary Stream file\n";
+    std::cout << "\t\".265\" => a H.265 Video Elementary Stream file\n";
+    std::cout << "\t\".aac\" => an AAC Audio (ADTS format) file\n";
+    std::cout << "\t\".ac3\" => an AC-3 Audio file\n";
+    std::cout << "\t\".amr\" => an AMR Audio file\n";
+    std::cout << "\t\".dv\" => a DV Video file\n";
+    std::cout << "\t\".m4e\" => a MPEG-4 Video Elementary Stream file\n";
+    std::cout << "\t\".mkv\" => a Matroska audio+video+(optional)subtitles file\n";
+    std::cout << "\t\".mp3\" => a MPEG-1 or 2 Audio file\n";
+    std::cout << "\t\".mpg\" => a MPEG-1 or 2 Program Stream (audio+video) file\n";
+    std::cout << "\t\".ogg\" or \".ogv\" or \".opus\" => an Ogg audio and/or video file\n";
+    std::cout << "\t\".ts\" => a MPEG Transport Stream file\n";
+    std::cout << "\t\t(a \".tsx\" index file - if present - provides server 'trick play' support)\n";
+    std::cout << "\t\".vob\" => a VOB (MPEG-2 video with AC-3 audio) file\n";
+    std::cout << "\t\".wav\" => a WAV Audio file\n";
+    std::cout << "\t\".webm\" => a WebM audio(Vorbis)+video(VP8) file\n";
+    std::cout << "See http://www.live555.com/mediaServer/ for additional documentation.\n";
 
     // Also, attempt to create a HTTP server for RTSP-over-HTTP tunneling.
     // Try first with the default HTTP port (80), and then with the alternative HTTP
@@ -76,13 +76,14 @@ int main(int argc, char** argv)
 
     if (rtspServer->setUpTunnelingOverHTTP(80) || rtspServer->setUpTunnelingOverHTTP(8000) || rtspServer->setUpTunnelingOverHTTP(8080)) 
     {
-        *env << "(We use port " << rtspServer->httpServerPortNum() << " for optional RTSP-over-HTTP tunneling).)\n";
+        std::cout << "(We use port " << rtspServer->httpServerPortNum() << " for optional RTSP-over-HTTP tunneling).)\n";
     }
     else 
     {
-        *env << "(RTSP-over-HTTP tunneling is not available.)\n";
+        std::cout << "(RTSP-over-HTTP tunneling is not available.)\n";
     }
 
+    // start server blocking this thread
     env->taskScheduler().doEventLoop(); // does not return
 
     return 0;
