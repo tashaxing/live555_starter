53c3cf4ce95657a75369d9b729233d19a460cff9
tashaxing <tashaxing123@163.com>
2022-01-26 16:11:07 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -21,7 +21,7 @@ int main(int argc, char** argv)
     // Create the RTSP server.  Try first with the default port number (554),
   // and then with the alternative port number (8554):
     RTSPServer* rtspServer;
-    portNumBits rtspServerPortNum = 554;
+    portNumBits rtspServerPortNum = 554; // NOTICE: can specify port
     rtspServer = DynamicRTSPServer::createNew(*env, rtspServerPortNum, authDB);
     if (rtspServer == NULL) {
         rtspServerPortNum = 8554;
